bac87b0b52446d0a804d197107cdb9f4a7342d13
harua <251zs00508@sudent.zen.ac.jp>
2026-08-14 17:20:47 +0900

    render対応

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const { serve } = require('@hono/node-server');
 const app = require('./app');
 
-const port = 3000;
-console.log(`Server running at http://localhost:${port}/`);
-serve({
-  fetch: app.fetch,
-  port,
-});
+const port = Number(process.env.PORT) || 3000;
+  console.log(`Server running at http://localhost:${port}/`);
+  serve({
+    fetch: app.fetch,
+    port,
+  });
